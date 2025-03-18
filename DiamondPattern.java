59b6550144cb7e60352b2871f5cd80c4596b27ae
stanlin <antonystanlin0@gmail.com>
2025-03-18 14:23:29 +0530

    pattern

---
+++ DiamondPattern.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class DiamondPattern{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int nums[][]=new int[n][n];
+        int start=n/2;
+        nums[0][start]=1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i>0){
+                    if(j>0)
+                        nums[i][j]+=nums[i-1][j-1];
+                    if(j<n-1)
+                        nums[i][j]+=nums[i-1][j+1];
+                }
+                if(j>=start && j<n-start)
+                    System.out.print(nums[i][j]+" ");
+                else
+                    System.out.print("  ");
+            }
+            System.out.println();
+            if(i<n/2)
+                start--;
+            else
+                start++;
+        }
+    }
+}
